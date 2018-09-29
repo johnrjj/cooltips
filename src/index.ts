decefb0b181c58687d8fa1c66ba560fafdb84a52
John Johnson <johnrjj@gmail.com>
2018-09-29 15:00:38 -0700

    let there be code

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { X } from './test';
+
+export {
+  X,
+};
---
+++ src/test.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class X extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: true,
+        }
+    }
+}
+
+export {
+    X,
+};
